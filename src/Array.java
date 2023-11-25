14eb55cb7f4ace9a4514085bcfd931cfc85b94fa
alvine2200 <alvinellavu@gmail.com>
2023-11-25 15:16:02 +0300

    arrays

---
+++ src/Array.java
@@ -0,0 +1,13 @@
+public class Array {
+    public static void main(String[] args){
+        int num[]=new int[4];
+        num[0]=1;
+        num[1]=2;
+        num[2]=3;
+        num[3]=4;
+
+        for(int i=0; i<=4; i++){
+            System.out.println(num[i]);
+        }
+    }
+}
