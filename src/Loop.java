6066f6975c5d5fe8b7e2448e344d2aebb2f5d67f
alvine2200 <alvinellavu@gmail.com>
2023-12-13 20:44:11 +0300

    loop

--- src/Loop.java
+++ src/Loop.java
@@ -9,5 +9,34 @@ public class Loop {
         {
             System.out.println(name);
         }
+
+        System.out.println(checkAge(21));
+        System.out.println(castingValues(2,8));
+        System.out.println(castDoubleToInt(20.0,20.0));
+    }
+
+
+    public static String  checkAge(int age)
+    {
+        if(age < 18)
+        {
+           return "Access Denied, Below Age";
+        }
+        else {
+            return "Your Age is " + age;
+        }
+    }
+
+    //casting values
+    public static double castingValues(int a, int b)
+    {
+        int sum=a+b;
+        return (double) sum;
+    }
+
+    public static int castDoubleToInt(double num1,double num2)
+    {
+        double multiplication=num1*num2;
+        return (int) multiplication;
     }
 }
